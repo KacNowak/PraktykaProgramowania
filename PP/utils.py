42c0a4680ace0c3fc0d913a2c3b6c0d33ad40ac1
KacNowak <kac.nowak.syr@gmail.com>
2024-05-15 12:55:21 +0200

    Add module docstring to utils.py

--- PP/utils.py
+++ PP/utils.py
@@ -1,12 +1,23 @@
-# przykladowe funkcje utils.py
-def add ( a: int , b: int) -> int:
+"""
+Moduł utils.py zawiera przykładowe funkcje matematyczne.
+"""
+
+
+def add(a: int, b: int) -> int:
+    """Dodaje dwie liczby."""
     return a + b
 
-def subtract ( a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """Odejmuje jedną liczbę od drugiej."""
     return a - b
 
-def multiply ( a: int , b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """Mnoży dwie liczby."""
     return a * b
 
-def divide ( a: int , b: int) -> float :
+
+def divide(a: int, b: int) -> float:
+    """Dzieli jedną liczbę przez drugą."""
     return a / b
